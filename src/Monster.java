0a4aa199e2c9f3f2b7f5b407498b8130c83c0902
JenJenUwU <jenjentw358@gmail.com>
2023-09-26 15:14:28 -0400

    class constructor in progress

--- src/Monster.java
+++ src/Monster.java
@@ -5,8 +5,48 @@ public class Monster {
     private String mType;
     private String mName;
     private int mAge;
-    private boolean badBreath;
-    private double gpa;
-    public Monster(){
+    private boolean mBreath;
+    private double mGPA;
+
+    /**
+     * Constructor w/ no parameters
+     */
+    public Monster() {
+        mType = "Zombie";
+        mName = "Uugggghhh";
+        mAge = 0;
+        mBreath = true;
+        mGPA = 3.9;
+    }
+
+    /**
+     * Constructor w/ name and age
+     *
+     * @param name Name of the monster object
+     * @param age  Age of the monster object
+     */
+    public Monster(String name, int age) {
+        mType = "Vampire";
+        mName = name;
+        mAge = age;
+        mBreath = false;
+        mGPA = //0.0 to 4.0;
+    }
+
+    /**
+     * Constructor w/ type,name,age,confirmation of bad breath, and gross point average
+     *
+     * @param type              Type of the monster object
+     * @param name              Name of the monster object
+     * @param age               Age of the monster object
+     * @param badBreath         Whether the monster object has bad breath or not
+     * @param grossPointAverage How gross the monster object is
+     */
+    public Monster(String type, String name, int age, boolean badBreath, double grossPointAverage) {
+        mType = type;
+        mName = name;
+        mAge = age;
+        mBreath = badBreath;
+        mGPA = grossPointAverage;
     }
 }
