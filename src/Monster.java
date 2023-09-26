19af0c437c20e0c03fa19806fc2aea1b8b022ca9
JenJenUwU <jenjentw358@gmail.com>
2023-09-26 14:51:14 -0400

    testing comments and rebase

---
+++ src/Monster.java
@@ -0,0 +1,13 @@
+public class Monster {
+    /**
+     * Class Instance Variables
+     */
+    private String mType;
+    private String mName;
+    private int mAge;
+    private boolean badBreath;
+    private double gpa;
+    public Monster(){
+        mType =
+    }
+}
